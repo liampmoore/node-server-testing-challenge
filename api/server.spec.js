fefa61ac9aadf0b9cca0fa43ee78d3b1cedccc01
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-26 18:02:55 -0400

    Completed MVP

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,8 +1,16 @@
 require('dotenv').config()
 
+const db = require('../database/db-Config.js');
+
 const request = require('supertest');
 
 const server = require('./server.js');
+
+beforeEach(async () => {
+    // this function executes and clears out the table before each test
+    await db('birds').truncate();
+  });
+
 
 describe('server.js', () => {
     it('Has NODE_ENV as testing.', () => {
@@ -22,22 +30,44 @@ describe('server.js', () => {
     })
     describe("Birds route", () => {
         describe("GET birds endpoint.", () => {
-            it.todo("Should return 200 status code.")
             it("Should return 200 status code.", () => {
                 return request(server).get("/api/birds")
                     .expect(200)
                     .expect('Content-Type', /json/i)
             })
-            it.todo("Should return an array.")
-            it.todo("Should return bird objects.")
+            it("Should return an array.", async () => {
+                const response = await request(server).get("/api/birds")
+                expect(response.body).toHaveLength(0)
+                expect(typeof(response.body)).toMatch(/object/)
+            })
         })
         describe("POST birds.", () => {
-            it.todo("Should return 201 status.")
-            it.todo("Should return the bird you posted.")
+            it("Should return 201 status.", async () => {
+                const bird = {name: "Turkey"}
+                const response = await request(server).post("/api/birds").send(bird)
+                expect(response.status).toBe(201);
+            })
+            it("Should return the bird you posted.", async () => {
+                const bird = {name: "Turkey"}
+                const response = await request(server).post("/api/birds").send(bird)
+                expect(response.body.name).toMatch(/Turkey/)
+            })
         })
         describe("GET bird with ID.", () => {
-            it.todo("Should return 200 status.")
-            it.todo("Should return the bird in question.")
+            it("Should return 200 status.", async () => {
+                const response = await request(server).get("/api/birds/1")
+                expect(response.status).toBe(200)
+            })
+            it("Should return the bird in question.", async () => {
+                const bird = {name: "Turkey"}
+                const postResponse = await request(server).post("/api/birds").send(bird)
+                const id = postResponse.body.id;
+
+                const getResponse = await request(server).get(`/api/birds/${id}`);
+
+                expect(getResponse.body.id).toBe(id)
+                expect(getResponse.body.name).toMatch(/turkey/i)
+            })
         })
             
     })
--- birds/birds-router.js
+++ birds/birds-router.js
@@ -16,4 +16,26 @@ router.get("/", async (req, res) => {
 
 })
 
+
+router.get("/:id", async (req, res) => {
+    try {
+        const bird = await Birds.findById(req.params.id)
+        res.status(200).json(bird)
+    }
+    catch(err) {
+        console.error(err)
+    }
+})
+
+router.post("/", async (req, res) => {
+    try {
+        const newBird = await Birds.insert(req.body);
+        res.status(201).json(newBird);
+    }
+    catch(err) {
+        console.error(err)
+        res.status(500).json({error: "Server error."})
+    }
+})
+
 module.exports = router;
